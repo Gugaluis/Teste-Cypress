76e4a57fd408cdeac629297deeabf2a1aa87e4e2
Luis Gustavo dos Santos <devluisantos@gmail.com>
2025-08-01 19:00:00 -0300

    ✨: Melhorias no código de visual e acessibilidade

--- cypress/e2e/checagem_visual_acessibilidade.cy.js
+++ cypress/e2e/checagem_visual_acessibilidade.cy.js
@@ -1,17 +1,37 @@
-describe("Kanban App - Testes de Acessibilidade e Visuais", () => {
+describe("Kanban App - Acessibilidade e Estrutura Visual", () => {
+  const baseUrl = "https://kanban-dusky-five.vercel.app/"
+
   beforeEach(() => {
-    cy.visit("https://kanban-dusky-five.vercel.app/")
-    cy.get("body").should("be.visible")
+    cy.visit(baseUrl)
+    cy.get("body", { timeout: 15000 }).should("be.visible")
   })
 
-  it("Contraste de cores", () => {
-    cy.get("body").should("have.css", "color")
-    cy.get("body").should("have.css", "background-color")
+  it("Deve exibir cores visíveis e definidas no body", () => {
+    cy.get("body").should(($body) => {
+      const bgColor = $body.css("background-color")
+      const textColor = $body.css("color")
+      expect(bgColor).to.not.be.empty
+      expect(textColor).to.not.be.empty
+    })
   })
 
-  it("Acessibilidade básica", () => {
-    cy.get('h1, h2, h3, [role="heading"]').should("exist")
+  it("Deve conter títulos visuais e colunas principais do Kanban", () => {
+    cy.contains("Quadro Kanban").should("be.visible")
+    cy.contains("To Do").should("be.visible")
+    cy.contains("In Progress").should("be.visible")
+    cy.contains("Done").should("be.visible")
+  })
 
-    cy.get("button, input, a, [tabindex]").should("have.length.at.least", 1)
+  it("Os cartões devem ser focáveis com tabindex", () => {
+    cy.get("[tabindex]").should("have.length.at.least", 1)
+    cy.get("[tabindex]").first().focus().should("have.attr", "tabindex")
+  })
+
+  it("Não deve conter imagens quebradas ou elementos <img> ausentes", () => {
+    cy.get("img").should("have.length", 0)
+  })
+
+  it("Não deve conter links (<a>), já que navegação é via SPA", () => {
+    cy.get("a").should("have.length", 0)
   })
 })
